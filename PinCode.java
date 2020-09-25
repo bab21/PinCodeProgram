b196d67929c7d03651b97c844b59cc4f3c888ab5
bab21 <2016061@iiitdmj.ac.in>
2020-09-25 20:55:15 +0530

    checking for last charater

--- PinCode.java
+++ PinCode.java
@@ -6,12 +6,12 @@ public class PinCode{
 		
 		Scanner s=new Scanner(System.in);
 
-		String regex="^[0-9]{6}";
+		String regex="^[0-9]{6}$";
 		Pattern p = Pattern.compile(regex); 
 		
 		
-		Matcher m1=p.matcher("A54026");
-		System.out.println("A54026 matches (true/false):"+m1.matches());
+		Matcher m1=p.matcher("85402B");
+		System.out.println("85402B matches (true/false):"+m1.matches());
 		
 		
 		
